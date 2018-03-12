3666b74cd371f4ba65bb496c6c65c11093c9f993
CHEN_ZJ <c297131019@users.noreply.github.com>
2018-03-12 14:03:17 +0800

    Initial commit

---
+++ app/src/main/java/com/test/chen/jbustest/BActivity.kt
@@ -0,0 +1,33 @@
+package com.test.chen.jbustest
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.test.jasonchen.assemblydemo.view.JBus
+import kotlinx.android.synthetic.main.activity_main.*
+
+class BActivity : AppCompatActivity(), JBus.OnEventMessageListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
+        JBus.registerEvent(this::class.java, this)
+        btn1.setOnClickListener {
+
+            JBus.sendMsg("All Hello")
+
+        }
+        btn2.setOnClickListener {
+            startActivity(Intent(this, AActivity::class.java))
+        }
+    }
+
+    override fun <T> onMsg(s: T) {
+        when (s) {
+            is String -> Toast.makeText(this, "I'm ${this::class.java.simpleName} msg = $s", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/test/chen/jbustest/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.test.chen.jbustest
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.test.jasonchen.assemblydemo.view.JBus
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), JBus.OnEventMessageListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        JBus.registerEvent(this::class.java, this)
+
+        btn1.setOnClickListener {
+            startActivity(Intent(this, AActivity::class.java))
+        }
+
+        btn2.setOnClickListener {
+            startActivity(Intent(this, BActivity::class.java))
+        }
+    }
+
+    override fun <T> onMsg(s: T) {
+        when (s) {
+            is String -> Toast.makeText(this, "I'm ${this::class.java.simpleName} msg = $s", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
